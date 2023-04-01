d889c283cf39f2bc03b772abe492b31b24ea7d91
Gupta_Rishi <108511606+MG-0070@users.noreply.github.com>
2023-04-01 23:07:58 +0530

    Add files via upload

---
+++ src/Components/BubbleChart.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Chart from "react-apexcharts";
+
+const BubbleChart = () => {
+  // bubble chart for second page to show monthly earning or engaging users
+  const options = {
+    chart: {
+      id: "basic-bar",
+    },
+    xaxis: {
+      categories: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept","Oct","Nov","Dec"],
+    },
+  };
+
+  const series = [
+    {
+      name: "series-1",
+      data: [30, 40, 45, 50, 49, 60, 70, 91,55,10,22,55],
+    },
+  ];
+
+  return (
+    <div className="bubblechart">
+      {/* bubble chart using apexchart */}
+      <Chart
+        options={options}
+        series={series}
+        type="scatter"
+        width="800"
+        height={"100%"}
+      />
+    </div>
+  );
+};
+
+export default BubbleChart;
